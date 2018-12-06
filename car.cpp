ff2c343c01e6d86fc16a166ac1782b8cd8c7819f
Huemer Patrick <pat.huemer@gmail.com>
2018-12-06 13:34:05 +0100

    f

--- car.cpp
+++ car.cpp
@@ -1,7 +1,6 @@
 #include "car.h"
 #include <stdbool.h>
 
-
 struct CarImplementation{
   int speed;
   enum Color color;
@@ -12,33 +11,48 @@ struct CarImplementation{
 
 };
 
-static CarImplementation aixam{0,RED,AIXAM,16.0,0.0,true};
-static CarImplementation jeep{0,RED,AIXAM,16.0,0.0,true};
-static CarImplementation fiat_multipla{0,GREEN,AIXAM,65.0,0.0,true};
+struct CarImplementation aixam{0,RED,AIXAM,16.0,0.0,true};
+struct CarImplementation jeep1{0,SILVER,JEEP,80.0,0.0,true};
+struct CarImplementation jeep2{0,BLACK,JEEP,80.0,0.0,true};
+struct CarImplementation fiat_multipla1{0,GREEN,FIAT_MULTIPLA,65.0,0.0,true};
+struct CarImplementation fiat_multipla2{0,BLUE,FIAT_MULTIPLA,65.0,0.0,true};
+struct CarImplementation fiat_multipla3{0,ORANGE,FIAT_MULTIPLA,65.0,0.0,true};
 
+Car car_park[6] = {&aixam, &jeep1, &jeep2, &fiat_multipla1, &fiat_multipla2, &fiat_multipla3};
 Car get_car(enum Type type){
+  for (int i = 0; i < 6; i++) {
+    if (car_park[i]->type==type&&car_park[i]->is_available==true) {
+      car_park[i]->is_available = false;
+      return car_park[i];
+    }
+  }
   return 0;
 }
 void set_acceleration_rate(Car car,double acceleration_rate){
 
 }
 int get_speed(Car car){
-  return 0;
+  return car->speed;
 }
 enum Color get_color(Car car){
-  return RED;
+  return car->color;
 }
 double get_fill_level(Car car){
-  return 0;
+  return car->fill_level;
 }
 enum Type get_type(Car car){
   return AIXAM;
 }
 void init(){
+  for (int i = 0; i < 6; i++) {
+    car_park[i]->acceleration_rate = 0;
+    car_park[i]->is_available = true;
+    car_park[i]->speed = 0;
 
+  }
 }
 double get_acceleration_rate(Car car){
-  return 0;
+  return car->acceleration_rate;
 }
 void accelerate(Car car){
 
