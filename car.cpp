29cd4d5b700abf69d75b770a753568a129215f56
PatrickHuemer <pat.huemer@gmail.com>
2018-12-10 19:43:05 +0100

    Last step

--- car.cpp
+++ car.cpp
@@ -3,20 +3,23 @@
 
 struct CarImplementation{
   int speed;
+  int max_speed;
   enum Color color;
   enum Type type;
   double fill_level;
   double acceleration_rate;
+  double lowest_acceleration_rate;
+  double highest_acceleration_rate;
   bool is_available;
 
 };
 
-struct CarImplementation aixam{0,RED,AIXAM,16.0,0.0,true};
-struct CarImplementation jeep1{0,SILVER,JEEP,80.0,0.0,true};
-struct CarImplementation jeep2{0,BLACK,JEEP,80.0,0.0,true};
-struct CarImplementation fiat_multipla1{0,GREEN,FIAT_MULTIPLA,65.0,0.0,true};
-struct CarImplementation fiat_multipla2{0,BLUE,FIAT_MULTIPLA,65.0,0.0,true};
-struct CarImplementation fiat_multipla3{0,ORANGE,FIAT_MULTIPLA,65.0,0.0,true};
+struct CarImplementation aixam{0,45,RED,AIXAM,16.0,0.0,-8.0,1.0,true};
+struct CarImplementation jeep1{0,196,SILVER,JEEP,80.0,0.0,-8.0,3.14,true};
+struct CarImplementation jeep2{0,196,BLACK,JEEP,80.0,0.0,-8.0,3.14,true};
+struct CarImplementation fiat_multipla1{0,170,GREEN,FIAT_MULTIPLA,65.0,0.0,-8.0,2.26,true};
+struct CarImplementation fiat_multipla2{0,170,BLUE,FIAT_MULTIPLA,65.0,0.0,-8.0,2.26,true};
+struct CarImplementation fiat_multipla3{0,170,ORANGE,FIAT_MULTIPLA,65.0,0.0,-8.0,2.26,true};
 
 Car car_park[6] = {&aixam, &jeep1, &jeep2, &fiat_multipla1, &fiat_multipla2, &fiat_multipla3};
 Car get_car(enum Type type){
@@ -29,7 +32,15 @@ Car get_car(enum Type type){
   return 0;
 }
 void set_acceleration_rate(Car car,double acceleration_rate){
-
+  if (acceleration_rate > car->highest_acceleration_rate) {
+    car->acceleration_rate = acceleration_rate;
+  }
+  else if (acceleration_rate < car->lowest_acceleration_rate) {
+    car->acceleration_rate = acceleration_rate;
+  }
+  else{
+    car->acceleration_rate = acceleration_rate;
+  }
 }
 int get_speed(Car car){
   return car->speed;
@@ -41,7 +52,7 @@ double get_fill_level(Car car){
   return car->fill_level;
 }
 enum Type get_type(Car car){
-  return AIXAM;
+  return car->type;
 }
 void init(){
   for (int i = 0; i < 6; i++) {
@@ -55,5 +66,12 @@ double get_acceleration_rate(Car car){
   return car->acceleration_rate;
 }
 void accelerate(Car car){
+  double speed = car->acceleration_rate * 3.6;
+  if (car->max_speed >= speed + car->speed) {
+    car->speed += speed;
+  }
+  else{
+    car->speed = car->max_speed;
+  }
 
 }
